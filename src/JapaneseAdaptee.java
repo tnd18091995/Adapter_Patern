3df0f3495ce851989b34067090c30f433c9e6cd1
tnd18091995 <tnd18091995@gmail.com>
2024-05-07 08:37:52 +0700

    AdaterPattern

---
+++ src/JapaneseAdaptee.java
@@ -0,0 +1,6 @@
+public class JapaneseAdaptee {
+    public void receive(String words){
+        System.out.println("Retrieving words form adapter...");
+        System.out.println(words);
+    }
+}
---
+++ src/VietnameseClient.java
@@ -0,0 +1,6 @@
+public class VietnameseClient {
+    public static void main(String[] args) {
+        VietnameseTarget client = new TranslatorAdapter(new JapaneseAdaptee());
+        client.send("Xin chao!");
+    }
+}
